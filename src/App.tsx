f92585712d55820207d44ae4fe361b6a0612f3e2
ahmadysalah <a.ahmad.salah@gmail.com>
2022-12-29 19:29:16 +0200

    update style main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-const App = () => {
+const App: React.FC = () => {
   return (
-    <div className="container">
-      <main className="main">
-        <h1>DRAWER</h1>
-        <div id="main_element_raper">
-          <h2>Drawer</h2>
-        </div>
+    <div className="app">
+      <div className="app_header">
+        <h2 className="app_title">DRAWER</h2>
+      </div>
+      <main className="app_main">
         <div className="main_board">
           <h2>board</h2>
+        </div>
+        <div className="main_drawer">
+          <h2>Drawer</h2>
         </div>
       </main>
     </div>
