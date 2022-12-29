f218d0ac8bfefcfa23dbb8e44fc34ab84bafea45
ahmad_salah <a.ahmad.salah@gmail.com>
2022-12-29 19:04:36 +0200

    create react app initialize react eco-system

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <main className="main">
+        <h1>DRAWER</h1>
+        <div id="main_element_raper">
+          <h2>Drawer</h2>
+        </div>
+        <div className="main_board">
+          <h2>board</h2>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
